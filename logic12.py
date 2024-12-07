d169c1a0b6cd58e16cf1a02b35b758880561edba
xakimovallamurod <133854536+xakimovallamurod@users.noreply.github.com>
2024-12-07 16:46:08 +0200

    Update logic12.py

--- logic12.py
+++ logic12.py
@@ -1,9 +1,9 @@
 def main(a):
     """
-    Given a two-digit integer a,  check the following statement "All digits of the number are the same".
-    Args:
-        a(int): parameter a
-    Returns:
-        bool: answer
+    Berilgan ikkita raqamli a sonida "Sonning barcha raqamlari bir xil" ekanligini tekshirish.
+    Argumentlar:
+        a(int): a parametri (ikkita raqamli son)
+    Qaytaradi:
+        bool: javob (True yoki False)
     """
-    return
+    return 
